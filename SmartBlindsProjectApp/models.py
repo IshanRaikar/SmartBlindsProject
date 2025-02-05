a7d35c37421b81c8c5a4b543c7d7114e089a115a
N7Ironclad <skinnyhan@gmail.com>
2025-02-05 12:18:05 -0600

    Added some imports to urls.py, views.py, and models.py Added model for Window and Room

--- SmartBlindsProjectApp/models.py
+++ SmartBlindsProjectApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+        roomId = models.AutoField(primary_key = True)
+        roomName = models.CharField(max_length=50, unique=True)
+        def __str__(self):
+                return f"Room {self.roomId}: {self.roomName}"
+
+class Window(models.Model):
+        DIRECTIONS = [
+                ('N', 'North'), ('NE', 'Northeast'), ('E', 'East'), ('SE', 'Southeast'),
+                ('S', 'South'), ('SW', 'Southwest'), ('W', 'West'), ('NW', 'Northwest')
+        ]
+
+        windowId = models.AutoField(primary_key=True)
+        facing = models.CharField(max_length=2, choices=DIRECTIONS, null=False, blank=False)
+        room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='windows')
+
+        def __str__(self):
+                return f"Window {self.windowId} facing {self.facing} in Room {self.room.roomName}"
--- SmartBlindsProjectApp/views.py
+++ SmartBlindsProjectApp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from SmartBlindsProjectApp import models
 
 # Create your views here.
